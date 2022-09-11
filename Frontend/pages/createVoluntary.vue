e77e6ae7e59d76c787b88980ce267cb0b30ff59b
Vanina Correa <vanina.correa@usach.cl>
2022-09-10 23:07:46 -0300

    feat: view of createVoluntary

--- Frontend/pages/createVoluntary.vue
+++ Frontend/pages/createVoluntary.vue
@@ -1,32 +1,89 @@
 <template>
-<!-- <form>
-<div class="form-group"> 
-    <label for="full_name_id" class="control-label">Nombre</label>
-    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingrese su nombre">
-</div>    
+<form>
+    <div class="container">
+        <h3>Crear un voluntario</h3>
+        <br>
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
+          <div class="form-group"> 
+              <label for="name" class="control-label">Nombre</label>
+              <input v-model = "formData.name" type="text" class="form-control" id="name"  placeholder="Ingrese su nombre">
+          </div>    
 
-<div class="form-group"> 
-    <label for="edad" class="control-label">Edad</label>
-    <input type="text" class="form-control" id="edad" name="edad" placeholder="Ingrese su edad">
-</div>                    
-                        
-<div class="form-group"> 
-    <label for="salud" class="control-label">Salud</label>
-    <input type="text" class="form-control" id="salud" name="salud" placeholder="Ingrese su estado de salud en la escala del 1 al 10">
-</div>    
+          <div class="form-group"> 
+              <label for="edad" class="control-label">Edad</label>
+              <input v-model = "formData.age" type="text" class="form-control" id="edad" name="edad" placeholder="Ingrese su edad">
+          </div>                    
+                                  
+          <div class="form-group"> 
+              <label for="salud" class="control-label">Salud</label>
+              <input v-model = "formData.health" type="text" class="form-control" id="salud" name="salud" placeholder="Ingrese su estado de salud en la escala del 1 al 10">
+          </div>    
 
-<div class="form-group"> 
-    <label for="equipamiento" class="control-label">Equipamiento</label>
-    <input type="text" class="form-control" id="equipamiento" name="equipamiento" placeholder="Ingrese el tipo de equipamiento que tiene a su disposición">
-</div>                                         
+          <div class="form-group"> 
+              <label for="equipamiento" class="control-label">Equipamiento</label>
+              <input v-model = "formData.inventory" type="text" class="forlm-control" id="equipamiento" name="equipamiento" placeholder="Ingrese el tipo de equipamiento que tiene a su disposición">
+          </div>                                         
 
-<div class="form-group"> 
-    <button type="submit" class="btn btn-primary">Registrarse</button>
-</div>     
+          <div class="form-group"> 
+              <button type="submit" v-on:click="sendData()" class="btn btn-primary">Registrarse</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</form> 
 
-</form> -->
 </template>
 <script>
-  export default {
+  import axios from 'axios'
+  export default{
+    data () {
+      return {
+          formData: {
+            name: '',
+            age: '',
+            health: '',
+            inventory: '',
+            avalaible: 1
+          }
+      }
+    },
+    methods:{
+        
+        
+        sendData: async function() {
+            try {
+                let response = await this.$axios.post('/voluntaries', this.formData);
+                console.log(this.formData);
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+            /*
+            await this.$axios.post('/voluntaries', {
+                name: this.nombre,
+                age: this.edad,
+                health: this.salud,
+                inventory: this.equipamiento
+            }).then(data => {
+                console.log(this.nombre)
+            })
+        }*/
+        /*
+      //Función asíncrona para consultar los datos
+      getData: async function(){
+        try {
+          let response = await this.$axios.post('/voluntaries');
+          this.items = response.data;
+          console.log(response)
+        } catch (error) {
+          console.log('error', error);
+        }
+      } */
+      },
+      //Función que se ejecuta al cargar el componente
+    //   created:function(){
+    //   this.sendData();
+    // }
   }
   </script>
